38062f504d8db76e3036be1992f3d5a5742b4475
mjaow <mjaow@sina.com>
2016-06-30 18:19:54 +0800

    add log for invoker

--- src/main/java/com/youku/rpc/invoker/impl/DefaultInvoker.java
+++ src/main/java/com/youku/rpc/invoker/impl/DefaultInvoker.java
@@ -1,5 +1,8 @@
 package com.youku.rpc.invoker.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.youku.rpc.client.Client;
 import com.youku.rpc.client.Request;
 import com.youku.rpc.exception.RpcException;
@@ -8,12 +11,15 @@ import com.youku.rpc.server.Response;
 
 public class DefaultInvoker extends AbstractInvoker {
 
+	private static final Logger log = LoggerFactory.getLogger(DefaultInvoker.class);
+
 	public DefaultInvoker(URL url, Client client, Class<?> interfaceClass) {
 		super(url, client, interfaceClass);
 	}
 
 	@Override
 	public Response invoke(Request request) throws RpcException {
+		log.info("向服务端{}发起请求", url.toString());
 		return client.send(request);
 	}
 
