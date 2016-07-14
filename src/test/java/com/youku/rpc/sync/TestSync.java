cc2f6145f9ce3ebc059d4d3aa1f06c4ac73fe548
mjaow <mjaow@sina.com>
2016-07-14 18:02:13 +0800

    add nio branch

---
+++ src/test/java/com/youku/rpc/sync/TestSync.java
@@ -0,0 +1,31 @@
+package com.youku.rpc.sync;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class TestSync {
+
+	private static ExecutorService service = Executors.newFixedThreadPool(10);
+
+	static int i = 0;
+
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		Future<Integer> f1 = null;
+		for (i = 0; i < 10; i++) {
+			f1 = service.submit(new Callable<Integer>() {
+
+				@Override
+				public Integer call() throws Exception {
+					Thread.sleep(5000);
+					return i;
+				}
+			});
+		}
+
+		System.out.println(f1.get());
+
+	}
+}
